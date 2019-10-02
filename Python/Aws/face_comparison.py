b07aa93d5de6f9ef12bb4b124ae8fc18dc99f722
mbermudez00 <mbermudes@ilsp.com.mx>
2019-10-02 17:12:57 -0500

    add aws test

--- Python/Aws/face_comparison.py
+++ Python/Aws/face_comparison.py
@@ -16,8 +16,8 @@ import boto3
 if __name__ == "__main__":
 
     # Replace sourceFile and targetFile with the image files you want to compare.
-    sourceFile='source.jpg'
-    targetFile='target.jpg'
+    sourceFile='img3.jpg'
+    targetFile='img4.jpg'
     client=boto3.client('rekognition')
    
     imageSource=open(sourceFile,'rb')
@@ -26,14 +26,16 @@ if __name__ == "__main__":
     response=client.compare_faces(SimilarityThreshold=70,
                                   SourceImage={'Bytes': imageSource.read()},
                                   TargetImage={'Bytes': imageTarget.read()})
+
+    print(response)
     
-    for faceMatch in response['FaceMatches']:
-        position = faceMatch['Face']['BoundingBox']
-        confidence = str(faceMatch['Face']['Confidence'])
-        print('The face at ' +
-               str(position['Left']) + ' ' +
-               str(position['Top']) +
-               ' matches with ' + confidence + '% confidence')
+    # for faceMatch in response['FaceMatches']:
+    #     position = faceMatch['Face']['BoundingBox']
+    #     confidence = str(faceMatch['Face']['Confidence'])
+    #     print('The face at ' +
+    #            str(position['Left']) + ' ' +
+    #            str(position['Top']) +
+    #            ' matches with ' + confidence + '% confidence')
 
     imageSource.close()
     imageTarget.close()               
