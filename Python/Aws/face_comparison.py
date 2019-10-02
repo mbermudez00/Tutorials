4f12f3cfda43ffea183bb1e68683964e4859e164
mbermudez00 <mbermudes@ilsp.com.mx>
2019-10-02 15:10:59 -0500

    add

---
+++ Python/Aws/face_comparison.py
@@ -0,0 +1,39 @@
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+
+if __name__ == "__main__":
+
+    # Replace sourceFile and targetFile with the image files you want to compare.
+    sourceFile='source.jpg'
+    targetFile='target.jpg'
+    client=boto3.client('rekognition')
+   
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+
+    response=client.compare_faces(SimilarityThreshold=70,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+    
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        confidence = str(faceMatch['Face']['Confidence'])
+        print('The face at ' +
+               str(position['Left']) + ' ' +
+               str(position['Top']) +
+               ' matches with ' + confidence + '% confidence')
+
+    imageSource.close()
+    imageTarget.close()               
